4f7cb0e800a93bcc140a26e3ed30379c80bd2bca
xuhongxuc <xuhongxuc@gmail.com>
2021-12-16 01:13:59 +0800

    add L17 Letter Combinations of a Phone Number

---
+++ leetcode/dfs/L17.scala
@@ -0,0 +1,34 @@
+object L17 {
+
+  private val map = Map[Char, String]('2' -> "abc", '3' -> "def", '4' -> "ghi",
+    '5' -> "jkl", '6' -> "mno", '7' -> "pqrs", '8' -> "tuv", '9' -> "wxyz")
+
+  private val res = collection.mutable.ListBuffer[String]()
+
+  def main(args: Array[String]): Unit = {
+    println(letterCombinations("23"))
+    res.clear()
+    println(letterCombinations(""))
+    res.clear()
+    println(letterCombinations("9"))
+    res.clear()
+    println(letterCombinations("555"))
+  }
+  // dfs
+  def letterCombinations(digits: String): List[String] = {
+    if (digits.isEmpty) return res.toList
+
+    dfs("", digits)
+    res.toList
+  }
+
+  private def dfs(conbination: String, remainDigits: String): Unit = {
+    if (remainDigits.length == 0) {
+      res.addOne(conbination)
+      return
+    }
+    for (c <- map(remainDigits(0))) {
+      dfs(conbination + c, remainDigits.substring(1))
+    }
+  }
+}
