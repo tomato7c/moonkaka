0e487f4b15d9457688042ea0a4310f08a9eb9b70
xuhongxuc <xuhongxuc@gmail.com>
2022-01-03 11:30:24 +0800

    update L83 Remove Duplicates from Sorted List

--- leetcode/list/L83.scala
+++ leetcode/list/L83.scala
@@ -1,23 +1,12 @@
-object L83 {
-
-  def main(args: Array[String]): Unit = {
-    deleteDuplicates(ListNode.of(Array(0, 0, 1, 1, 1, 5))).printListNode()
-  }
-
-  // 双指针，i维护结果集末尾, j向后移动 TODO 其实不需要dummy，head值是固定的
-  def deleteDuplicates(head: ListNode): ListNode = {
-    val dummy = new ListNode()
-    dummy.next = head
-    var p = dummy
-    var q = head
-    while (q != null) {
-      if (p.x != q.x || p == dummy) {
-        p.next = q
-        p = p.next
-      }
-      q = q.next
+object Solution {
+	// pre维护结果集末尾
+    def deleteDuplicates(head: ListNode): ListNode = {
+        var pre = head; var cur = head
+        while (cur != null) {
+            while (cur != null && cur.x == pre.x) cur = cur.next
+            pre.next = cur
+            pre = pre.next
+        }
+        head
     }
-    p.next = null
-    dummy.next
-  }
 }
